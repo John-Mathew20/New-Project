29a37daa6f32d801eaba901bf28a0dbcadcaf151
John Mathew14 <JohnMathew141105@gmail.com>
2026-03-12 21:44:27 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+async function changeColor(color, delay) {
+  let h1 = document.querySelector("h1");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      h1.style.color = color;
+      resolve("color Changed!");
+    }, delay);
+  });
+}
+
+async function show() {
+  try {
+    await changeColor("Red", 1000);
+    await changeColor("pink", 1000);
+    await changeColor("green", 1000);
+    await changeColor("aqua", 1000);
+    await changeColor("cyan", 1000);
+    await changeColor("lime", 1000);
+    await changeColor("maroon", 1000);
+    await changeColor("lavender", 1000);
+  } catch (e) {
+    console.log("Error Happened, Code Exited Without excution");
+  }
+}
+
+show();
